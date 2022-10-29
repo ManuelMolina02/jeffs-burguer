ad1c9aae3400f63f24323b9693b9ffd81da3a3d5
Manuel Molina <manuelanmolina@gmail.com>
2022-10-29 09:25:55 -0300

    testando build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import Head from 'next/head';
 import { Banner } from '../components/Banner';
 import { Header } from '../components/Header';
@@ -6,11 +6,10 @@ import { Highlight } from '../components/Highlight';
 import { Menu } from '../components/Menu';
 import { Footer } from '../components/Footer';
 import { Products } from '../components/Products';
-import { GetServerSideProps, GetStaticProps } from 'next';
+import { GetServerSideProps } from 'next';
 import { api } from '../services/api';
 
 export default function Home({ dataCards }) {
-
 
   return (
     <>
@@ -34,116 +33,7 @@ export default function Home({ dataCards }) {
 
 export const getServerSideProps: GetServerSideProps = async () => {
 
-  const response = [
-    {
-      id: 1,
-      name: "Costela Premium",
-      type: "Hambúrguer",
-      price: 26.0,
-      image: "/lanches/costela-premium.jpeg",
-      ingredients:
-        "Hambúrguer Artesanal 150g, Bacon, Queijo, Alface, Tomate, Cebola, Maionese",
-    },
-    {
-      id: 2,
-      name: "Nacho Burger",
-      type: "Hambúrguer",
-      price: 30.0,
-      image: "/lanches/nacho-burguer.jpeg",
-      ingredients:
-        "Hambúrguer Artesanal 150g, Bacon, Queijo, Alface, Tomate, Cebola, Maionese",
-    },
-    {
-      id: 3,
-      name: "4 Queijos Burguer",
-      type: "Hambúrguer",
-      price: 30.0,
-      image: "/lanches/quatro-queijos.jpeg",
-      ingredients:
-        "Hambúrguer Artesanal 150g, Bacon, Queijo, Alface, Tomate, Cebola, Maionese",
-    },
-    {
-      id: 4,
-      name: "Crocante",
-      type: "Hambúrguer",
-      price: 28.0,
-      image: "/lanches/crocante.jpeg",
-      ingredients:
-        "Hambúrguer Artesanal 150g, Bacon, Queijo, Alface, Tomate, Cebola, Maionese",
-    },
-    {
-      id: 4,
-      name: "Provolone",
-      type: "Hambúrguer",
-      image: "/lanches/provolone.jpeg",
-      price: 30.0,
-      ingredients:
-        "Hambúrguer Artesanal 150g, Bacon, Queijo, Alface, Tomate, Cebola, Maionese",
-    },
-    {
-      id: 5,
-      name: "Smash Júnior",
-      type: "Smash",
-      price: 16.0,
-      image: "/lanches/smash-junior.jpeg",
-      ingredients:
-        "Hambúrguer Artesanal 150g, Bacon, Queijo, Alface, Tomate, Cebola, Maionese",
-    },
-    {
-      id: 6,
-      name: "Smash Salada",
-      type: "Smash",
-      price: 18.0,
-      image: "/lanches/smash-salada.jpeg",
-      ingredients:
-        "Hambúrguer Artesanal 150g, Bacon, Queijo, Alface, Tomate, Cebola, Maionese",
-    },
-    {
-      id: 7,
-      name: "Jack Bacon",
-      type: "Hambúrguer",
-      image: "/lanches/jack-bacon.jpeg",
-      price: 25.0,
-      ingredients:
-        "Hambúrguer Artesanal 150g, Bacon, Queijo, Alface, Tomate, Cebola, Maionese",
-    },
-    {
-      id: 8,
-      name: "Jack Rodeio",
-      type: "Hambúrguer",
-      image: "/lanches/jack-rodeio.jpeg",
-      price: 25.0,
-      ingredients:
-        "Hambúrguer Artesanal 150g, Bacon, Queijo, Alface, Tomate, Cebola, Maionese",
-    },
-    {
-      id: 9,
-      name: "Hot Dog 1",
-      type: "Hot Dog",
-      price: 18.0,
-      image: "/lanches/hot-dog-um.jpeg",
-      ingredients:
-        "Hambúrguer Artesanal 150g, Bacon, Queijo, Alface, Tomate, Cebola, Maionese",
-    },
-    {
-      id: 10,
-      name: "Hot Dog 2",
-      type: "Hot Dog",
-      price: 16.0,
-      image: "/lanches/hot-dog-dois.jpeg",
-      ingredients:
-        "Hambúrguer Artesanal 150g, Bacon, Queijo, Alface, Tomate, Cebola, Maionese",
-    },
-    {
-      id: 11,
-      name: "Hot Dog 3",
-      type: "Hot Dog",
-      image: "/lanches/hot-dog-tres.jpeg",
-      price: 19.0,
-      ingredients:
-        "Hambúrguer Artesanal 150g, Bacon, Queijo, Alface, Tomate, Cebola, Maionese",
-    },
-  ];
+  const response = await api.get('/data').then(response => response.data)
 
   const dataCards = response.map(item => {
     return {
