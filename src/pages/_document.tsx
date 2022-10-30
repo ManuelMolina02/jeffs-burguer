f11e2a06f78c12a1d90433fc4f776be61a1fec94
Manuel Molina <manuelanmolina@gmail.com>
2022-10-30 17:55:27 -0300

    melhorando pontos de SEO na aplicação

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default class MyDocument extends Document {
     return (
       <Html lang='pt-BR'>
         <Head>
+          <meta name="description" content="Os melhores lanches da região estão aqui! Confira nossas opções." />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
