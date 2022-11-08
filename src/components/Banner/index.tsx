50152dd056500cd5ad2c21dceb9ce51cf4045e62
Manuel Molina <manuelanmolina@gmail.com>
2022-11-08 19:17:16 -0300

    atualizando fonte

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -16,20 +16,20 @@ export function Banner({ data }) {
 
       <Flex width='550px' className={styles.bannerContent} >
         <Flex flexDir='column' align='center'>
-          <Heading fontSize={'126px'} height='140px' fontFamily='Bebas Neue'>
+          <Heading fontSize={'126px'} height='140px'>
             TE VER
           </Heading>
-          <Text fontSize={'40px'} letterSpacing='.26em' fontFamily='Bebas Neue'>
+          <Text fontSize={'40px'} letterSpacing='.26em'>
             E NÃO TE QUERER
           </Text>
         </Flex>
 
         <Flex color='#fcde4a' textShadow={' 3px 9px 9px rgba(0, 0, 0, 0.72)'} flexDir='column' align='flex-start' fontSize='76px' fontWeight={'bold'} >
-          <Text height='140px' fontSize={'126px'} fontFamily='Bebas Neue'>
+          <Text height='140px' fontSize={'126px'}>
             É IMPROVÁVEL
           </Text>
 
-          <Text fontSize={'126px'} fontFamily='Bebas Neue'>
+          <Text fontSize={'126px'}>
             É IMPOSSÍVEL.
           </Text>
         </Flex>
