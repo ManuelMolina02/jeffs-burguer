bc46731abbf770525e28e145260de8362854da95
Manuel Molina <manuelanmolina@gmail.com>
2022-11-08 20:04:43 -0300

    ajustando banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -14,27 +14,29 @@ export function Banner({ data }) {
       h={"660px"}
     >
 
-      <Flex width='550px' className={styles.bannerContent} >
-        <Flex flexDir='column' align='center'>
-          <Heading fontSize={'126px'} height='140px' fontFamily={'Bebas Neue, cursive'}>
-            TE VER
-          </Heading>
-          <Text fontSize={'40px'} letterSpacing='.26em' fontFamily={'Bebas Neue, cursive'}>
-            E NÃO TE QUERER
-          </Text>
+      <main>
+        <Flex width='550px' className={styles.bannerContent}>
+          <Flex flexDir='column' align='center'>
+            <Heading fontSize={'126px'} height='140px'>
+              TE VER
+            </Heading>
+            <Text fontSize={'40px'} letterSpacing='.26em'>
+              E NÃO TE QUERER
+            </Text>
+          </Flex>
+
+          <Flex color='#fcde4a' textShadow={' 3px 9px 9px rgba(0, 0, 0, 0.72)'} flexDir='column' align='flex-start' fontSize='76px' fontWeight={'bold'} >
+            <Text height='140px' fontSize={'126px'}>
+              É IMPROVÁVEL
+            </Text>
+
+            <Text fontSize={'126px'}>
+              É IMPOSSÍVEL.
+            </Text>
+          </Flex>
+
         </Flex>
-
-        <Flex color='#fcde4a' textShadow={' 3px 9px 9px rgba(0, 0, 0, 0.72)'} flexDir='column' align='flex-start' fontSize='76px' fontWeight={'bold'} >
-          <Text height='140px' fontSize={'126px'}>
-            É IMPROVÁVEL
-          </Text>
-
-          <Text fontSize={'126px'}>
-            É IMPOSSÍVEL.
-          </Text>
-        </Flex>
-
-      </Flex >
+      </main>
 
     </Flex >
   )
